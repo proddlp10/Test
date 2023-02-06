f48c6d0ec459c1a37ebe4af92638ed4227c7e8e6
proddlp10 <proddlp10@gmail.com>
2023-02-05 23:12:54 -0800

    Create ccn.c

---
+++ ccn.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int a;
+    long b;   // equivalent to long int b;
+    long long c;  // equivalent to long long int c;
+    double e;
+    long double f;
+
+    printf("Size of int = %zu bytes \n", sizeof(a));
+    printf("Size of long int = %zu bytes\n", sizeof(b));
+    printf("Size of long long int = %zu bytes\n", sizeof(c));
+    printf("Size of double = %zu bytes\n", sizeof(e));
+    printf("Size of long double = %zu bytes\n", sizeof(f));
+    ccn		American Express,Amex,Master Card,Visa,CVV Code,CVV Number,CVC Code,CVC Number,Select Card Type,Discover,Diners Club,JCB,Pay with checking account,Pay check money order,Credit Card Number,Card holder Name,Expiration Date
+
+2264 2231 2829 8234		
+4744 9891 1362 0055		
+2262 3841 7879 8804		
+2314 6859 7722 8845		
+2222 4000 7000 0005		
+5555 3412 4444 1115		
+4360 0000 0100 0005		
+4929-3813-3266-4295		
+5370-4638-8881-3020		
+4916-4811-5814-8111		
+
+    
+    return 0;
+}
